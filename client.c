bd997042964c3a342b79d893fb5fb5253949d501
bond <weplaysoft@163.com>
2013-12-31 09:59:17 +0800

    second commit and build a framework of chat room

---
+++ client.c
@@ -0,0 +1,12 @@
+//This is the client of chat room
+#include <stdio.h>
+
+#ifndef DEBUG_CLIENT
+#define DEBUG_CLIENT
+#endif
+
+int main(int argc, const char *argv[])
+{
+		
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,12 @@
+//This is the server of chat room
+#include <stdio.h>
+
+#ifndef DEBUG_SERVER
+#define DEBUG_SERVER
+#endif
+
+int main(int argc, const char *argv[])
+{
+		
+	return 0;
+}
