eee9be646bfd3459e063d4469a278d7de51dc36b
bond <weplaysoft@163.com>
2014-01-03 17:23:58 +0800

    modified two .c files

--- client.c
+++ client.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 #ifndef DEBUG_CLIENT
-#define DEBUG_CLIENT
+#define DEBUG_CLIENT 1
 #endif
 
 int main(int argc, const char *argv[])
--- server.c
+++ server.c
@@ -1,12 +1,23 @@
 //This is the server of chat room
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 #ifndef DEBUG_SERVER
-#define DEBUG_SERVER
+#define DEBUG_SERVER 1
 #endif
 
 int main(int argc, const char *argv[])
 {
-		
+	int sockfd;
+   //    int socket(int domain, int type, int protocol);
+     if (( sockfd =socket(AF_INET,SOCK_STREAM , 0)) == -1 )
+	 {
+	 	printf ("socket is failed\n");
+		exit(0);
+	 }	 
+//int bind(int sockfd, const struct sockaddr *addr   socklen_t addrlen);
+
+	if ( bind( sockfd, 
 	return 0;
 }
